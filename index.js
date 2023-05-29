bc2dc3bfdbbcf46ef92385f9adf0e58aff6f5dca
codegrann <omondihills02@gmail.com>
2023-05-29 07:07:44 +0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("It works");
+let followCursor=document.querySelector("#follow");
+let container=document.querySelector("#txt");
+container.onhover=function(e){
+    
+}
+console.log(container)
