a3ff264de4c01cd1ec3168d4459de39ac783b858
codegrann <omondihills02@gmail.com>
2023-05-31 11:35:33 +0300

    .

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ let container=document.querySelector("#txt");
 // }
 
 container.onmouseover=function(e){
-    console.log(e.target);
-    console.log(e.clientX , e.clientY);
-    followCursor.style.left=e.clientX;
+    // console.log(e.target);
+    let xPos=e.clientX;
+    let yPos=e.clientY;
+    let red="red";
+    console.log(xPos , yPos);
+    followCursor.style.left=`${xPos}`;
+    followCursor.style.top=`${xPos}`;
+    followCursor.style.color=`${red}`;
 }
 
 
