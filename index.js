1681afd56f182bb80113ade4d5f84caefeff90fb
codegrann <omondihills02@gmail.com>
2023-05-31 12:35:09 +0300

    .

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-let followCursor=document.querySelector("#follow");
-let container=document.querySelector("#txt");
+let followCursor = document.querySelector("#follow");
+let container = document.querySelector("#txt");
 // followCursor.onmouseout=function(e){
-    // console.log(e.target);
-    // console.log(e.clientX , e.clientY);
+// console.log(e.target);
+// console.log(e.clientX , e.clientY);
 // }
 
-container.onmouseover=function(e){
+container.onmouseover = function (e) {
     // console.log(e.target);
-    let xPos=e.clientX;
-    let yPos=e.clientY;
-    let red="red";
-    console.log(xPos , yPos);
-    followCursor.style.left=`${xPos}`;
-    followCursor.style.top=`${xPos}`;
-    followCursor.style.color=`${red}`;
+    let xPos = e.clientX;
+    let yPos = e.clientY * 0.157480;
+    console.log(xPos, yPos);
+    followCursor.style.left = `${xPos}`;
+    followCursor.style.top = `(${yPos})px`;
+    followCursor.style.color = `red`;
 }
 
 
