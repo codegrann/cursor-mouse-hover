c353770725de0a8b104a26d02f62d98048a31738
codegrann <omondihills02@gmail.com>
2023-05-31 11:16:09 +0300

    .

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let followCursor=document.querySelector("#follow");
 let container=document.querySelector("#txt");
-followCursor.onmouseout=function(e){
+// followCursor.onmouseout=function(e){
+    // console.log(e.target);
+    // console.log(e.clientX , e.clientY);
+// }
+
+container.onmouseout=function(e){
     console.log(e.target);
     console.log(e.clientX , e.clientY);
 }
+
 
 console.log("It is not working");
