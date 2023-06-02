42f2fe33734c80347e2539db8a53f6a6b890d175
codegrann <omondihills02@gmail.com>
2023-06-02 15:49:55 +0300

    .

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ let container = document.querySelector("#txt");
 // console.log(e.target);
 // console.log(e.clientX , e.clientY);
 // }
-
-container.onclick = function (e) {
+container.addEventListener('mousemove', move)
+container.addEventListener('mouseover', move)
+container.addEventListener('click', move)
+function move (e) {
     // console.log(e.target);
     let xPos = e.clientX*0.073333;
     let yPos = e.clientY * 0.157480;
     console.log(xPos, yPos);
-    followCursor.style.left = `${xPos}%`;
     followCursor.style.top = `${yPos}%`;
+    followCursor.style.left = `${xPos}%`;
     followCursor.style.color = `red`;
 }
 
